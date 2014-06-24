76befcf27f5e5155475879c06b14ee1f6df9f86c
Simone Richardson <enomiswebit@gmail.com>
2014-06-24 16:58:21 -0600

    checking in

--- thedisemvoweler/Program.cs
+++ thedisemvoweler/Program.cs
@@ -10,6 +10,55 @@ namespace thedisemvoweler
     {
         static void Main(string[] args)
         {
+           
+            //Create a function called Disemvoweler that takes a string parameter called "input"
+      
+            
+            
+            //keep the console open
+            Console.ReadKey();
+
         }
+          
+        
+        
+        
+        static void Disemvoweler(string input)
+            {
+            //display the original input
+            
+            
+            {
+
+            //grabbing one letter from the string by its index
+            char vowels = input[i];
+
+            // hold our output
+            string vowels = "aeiou";
+
+
+            //declaring
+            int numVowels = 0;
+            for (int i = 0; i < input.Length; i=i+1)
+
+           //is it a vowel
+            if (letter [i] == 'a' || letter[i] == 'e' || letter[i] == 'i' || letter[i] == 'o' || letter[i] == 'u'){
+              
+
+                //found a vowel remove the vowel
+
+                else if ( letter == ' ')
+
+            }
+         }
     }
-}
+
+      
+            
+            
+            
+            
+            
+        
+    
+
---
+++ variables/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Humberto";
+            name = "Daniel";
+            name = "Simone";
+
+            bool looping = true;
+            while (looping);
+            }
+                Console.WriteLine("I'm looping");
+    {
+
+
+
+
+   
+                    looping = false;
+        }
+        }
+        // ADDING A DOUBLE VARIABLE TO 
+        //AN INT VARIABLE :
+
+        int anInt =  5
+double aDouble = 3.5;
+int total  = anInt + aDouble;
+    }
+        Console.ReadKey
+    }
